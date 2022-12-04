5259837a755e0660b50825c1952f4df40852a0c6
Anthony Huang <Huang.Anthony@outlook.com>
2022-12-04 15:27:31 -0500

    Added named location functionality
    + Added handler for city and country code to get weather data
    + Updated Regex to fit openweathermap's criterias

--- WeatherApp/Models/WeatherRequestDTO.cs
+++ WeatherApp/Models/WeatherRequestDTO.cs
@@ -4,8 +4,11 @@ namespace WeatherApp.Models
 {
     public class WeatherRequestDTO
     {
-        [RegularExpression("^[a-zA-Z\\s]+$", ErrorMessage = "City name can only contain alphabets and space characters.")]
+        [RegularExpression("^[a-zA-Z\\s]+$", ErrorMessage = "City name can only contain alphabet and space characters.")]
         public String? City { get; set; }
+
+        [RegularExpression("^[a-zA-Z\\s]{2}$", ErrorMessage = "Country name can only contain alphabet characters with 2 spaces in length.")]
+        public String? CountryCode { get; set; }
 
         [Range(-90, 90, ErrorMessage = "Valid latitude value between (-90, 90)")]
         public decimal? Lat { get; set; }
--- WeatherApp/Models/WeatherService.cs
+++ WeatherApp/Models/WeatherService.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Net;
+using System.Runtime.CompilerServices;
 using System.Text.Json.Nodes;
 using WeatherApp.Controllers;
 
@@ -27,70 +30,25 @@ namespace WeatherApp.Models
 
             if (!(weatherRequestDTO.Lon == null || weatherRequestDTO.Lat == null))
             {
-                decimal lat = Math.Round((decimal)weatherRequestDTO.Lat, 2);
-                decimal lon = Math.Round((decimal)weatherRequestDTO.Lon, 2);
 
                 _logger.LogInformation("Location Request");
+
+                decimal lat = Math.Round((decimal)weatherRequestDTO.Lat, 2);
+                decimal lon = Math.Round((decimal)weatherRequestDTO.Lon, 2);
                 string url = $"{baseUrl}/data/2.5/forecast?lat={lat}" +
                     $"&lon={lon}&cnt={cnt}&appid={apiKey}";
 
                 try
                 {
-                    using (var httpClient = _httpClientFactory.CreateClient())
+                    var data = await this.requestWeatherDataFromApi(url);
+                    if (data != null)
                     {
-                        HttpResponseMessage res = await httpClient.GetAsync(url);
-                        if (res.IsSuccessStatusCode)
-                        {
-                            string responseString = await res.Content.ReadAsStringAsync();
-                            var result = JsonObject.Parse(responseString);
-                            
-                            if (result == null)
-                            {
-                                _logger.LogInformation("API returned empty json.");
-                                // TODO: Throw error for no content
-                                throw new Exception();
-                            }
-
-                            var data = result["list"];
-
-                            if (data == null)
-                            {
-                                _logger.LogInformation("API returned empty list of data.");
-                                // TODO: Throw error for no content
-                                throw new Exception();
-                            }
-
-                            List<WeatherDTO> list = new List<WeatherDTO>();
-
-                            var dataArray = (JsonArray) data;
-                            foreach (var element in dataArray)
-                            {
-                                WeatherDTO weatherDTO = new WeatherDTO();
-                                weatherDTO.Temp = (decimal)element["main"]["temp"];
-                                weatherDTO.TempMin = (decimal)element["main"]["temp_min"];
-                                weatherDTO.TempMax = (decimal)element["main"]["temp_max"];
-                                weatherDTO.Humidity = (int)element["main"]["humidity"];
-                                if (element["weather"] != null)
-                                {
-                                    weatherDTO.Weather = (string)element["weather"][0]["main"];
-                                    weatherDTO.Description = (string)element["weather"][0]["description"];
-                                }
-                                if (element["clouds"] != null)
-                                {
-                                    weatherDTO.Cloud = (int)element["clouds"]["all"];
-                                }
-                                weatherDTO.Time = (string)element["dt_txt"];
-                                list.Add(weatherDTO);
-                            }
-                            return list;
-                        }
-                        else
-                        {
-                            return Enumerable.Empty<WeatherDTO>();
-                        }
+                        return data;
                     }
-                    return Enumerable.Empty<WeatherDTO>();
-
+                    else
+                    {
+                        return Enumerable.Empty<WeatherDTO>();
+                    }
                 }
                 catch (Exception e) 
                 {
@@ -99,11 +57,30 @@ namespace WeatherApp.Models
 
                 return Enumerable.Empty<WeatherDTO>();
             }
-            else if (!String.IsNullOrEmpty(weatherRequestDTO.City))
+            else if (!(String.IsNullOrEmpty(weatherRequestDTO.City) || String.IsNullOrEmpty(weatherRequestDTO.CountryCode)))
             {
                 _logger.LogInformation("City Request");
-                string url = $"{baseUrl}/geo/1.0/direct?q={weatherRequestDTO.City}&limit={1}&appid={apiKey}";
 
+                string countryCode = (string)weatherRequestDTO.CountryCode;
+                string city = (string)weatherRequestDTO.City.ToUpper();
+                string url = $"{baseUrl}/geo/1.0/direct?q={city},{countryCode}&limit={1}&appid={apiKey}";
+
+                try
+                {
+                    var data = await requestWeatherDataFromApiUsingNames(url, cnt);
+                    if (data != null)
+                    {
+                        return data;
+                    }
+                    else
+                    {
+                        return Enumerable.Empty<WeatherDTO>();
+                    }
+                }
+                catch(Exception e)
+                {
+                    _logger.LogError(e.ToString());
+                }
                 return Enumerable.Empty<WeatherDTO>();
             }
             else
@@ -116,6 +93,102 @@ namespace WeatherApp.Models
         public IEnumerable<String> GetListCities()
         {
             return new string[] {"city_test", "city_test_2"};
+        }
+
+        private async Task<IEnumerable<WeatherDTO>> requestWeatherDataFromApi(string url)
+        {
+            using (var httpClient = _httpClientFactory.CreateClient())
+            {
+                HttpResponseMessage res = await httpClient.GetAsync(url);
+                if (res.IsSuccessStatusCode)
+                {
+                    string responseString = await res.Content.ReadAsStringAsync();
+                    var result = JsonObject.Parse(responseString);
+
+                    if (result == null)
+                    {
+                        _logger.LogInformation("API returned empty json.");
+                        // TODO: Throw error for no content
+                        throw new Exception();
+                    }
+
+                    var data = result["list"];
+
+                    if (data == null)
+                    {
+                        _logger.LogInformation("API returned empty list of data.");
+                        // TODO: Throw error for no content
+                        throw new Exception();
+                    }
+
+                    List<WeatherDTO> list = new List<WeatherDTO>();
+
+                    var dataArray = (JsonArray)data;
+                    foreach (var element in dataArray)
+                    {
+                        WeatherDTO weatherDTO = new WeatherDTO();
+                        weatherDTO.Temp = (decimal)element["main"]["temp"];
+                        weatherDTO.TempMin = (decimal)element["main"]["temp_min"];
+                        weatherDTO.TempMax = (decimal)element["main"]["temp_max"];
+                        weatherDTO.Humidity = (int)element["main"]["humidity"];
+                        if (element["weather"] != null)
+                        {
+                            weatherDTO.Weather = (string)element["weather"][0]["main"];
+                            weatherDTO.Description = (string)element["weather"][0]["description"];
+                        }
+                        if (element["clouds"] != null)
+                        {
+                            weatherDTO.Cloud = (int)element["clouds"]["all"];
+                        }
+                        weatherDTO.Time = (string)element["dt_txt"];
+                        list.Add(weatherDTO);
+                    }
+                    return list;
+                }
+                else
+                {
+                    return Enumerable.Empty<WeatherDTO>();
+                }
+            }
+        }
+
+        private async Task<IEnumerable<WeatherDTO>> requestWeatherDataFromApiUsingNames(string url, int cnt)
+        {
+            using (var httpClient = _httpClientFactory.CreateClient())
+            {
+                HttpResponseMessage res = await httpClient.GetAsync(url);
+                if (res.IsSuccessStatusCode)
+                {
+                    string responseString = await res.Content.ReadAsStringAsync();
+                    var result = JsonObject.Parse(responseString);
+
+                    if(result == null)
+                    {
+                        return Enumerable.Empty<WeatherDTO>();
+                    }
+
+                    if (result[0]["lat"] == null || result[0]["lon"] == null)
+                    {
+                        return Enumerable.Empty<WeatherDTO>();
+                    }
+                    else
+                    {
+                        string baseUrl = _config["WeatherApi:Url"];
+                        string apiKey = _config["WeatherApi:ServiceApiKey"];
+                        decimal lat = (decimal)result[0]["lat"];
+                        decimal lon = (decimal)result[0]["lon"];
+
+                        url = $"{baseUrl}/data/2.5/forecast?lat={lat}" +
+                    $"&lon={lon}&cnt={cnt}&appid={apiKey}";
+
+                        return await this.requestWeatherDataFromApi(url);
+                    }
+                }
+                else
+                {
+                    return Enumerable.Empty<WeatherDTO>();
+                }
+            }
         }
     }
 
